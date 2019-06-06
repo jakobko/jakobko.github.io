fe6b35345cd9686a902ae2e95b3945428967a02f
jakobko <jakobko@matnat.student.uio.no>
2019-06-06 09:47:51 +0200

    new

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function endcoffee() {
+    document.getElementById("warmmug").style.display = "none";
+    document.getElementById("coldmug").style.display = "block";
+}
+
+function opentoclosed() {
+    document.getElementById("openmac").style.display = "none";
+    document.getElementById("closedmac").style.display = "block";
+}
+
+function closedtoopen() {
+    document.getElementById("closedmac").style.display = "none";
+    document.getElementById("openmac").style.display = "block";
+}
